df828115a60f733cc0a3c4ba64b8b4b4a737c5f7
syahmmidev <wanahmadsyahmi26@gmail.com>
2022-09-11 13:18:57 +0800

    finish

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const [users, setUsers] = useState([]);
+    let navigate = useNavigate()
+
+    async function fetchUser(){
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setUsers(data)
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, []);
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="user-list">
+                {users.map(user => (
+                    <div className="user" key={user.id} onClick={() => navigate(`${user.id}`)}>
+                        <div className="user-card">
+                            <div className="user-card__container">
+                                <h3>{user.name}</h3>
+                                <p>
+                                    <b>Email:</b> {user.email}
+                                </p>
+                                <p>
+                                    <b>Phone:</b> {user.phone}
+                                </p>
+                                <p>
+                                <b>Website:</b>
+                                    {user.website}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
